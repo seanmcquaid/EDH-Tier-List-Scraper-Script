4387c3229a556afdcb04fa631b305b463f97dce6
seanmcquaid <sean.mcquaid2@verizon.net>
2021-04-23 16:21:43 -0400

    finished initial script, just need to add in taking text from command line

---
+++ index.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const url = 'https://tappedout.net/mtg-decks/best-commanders-in-edh-tier-list/';
+
+const searchedString = 'Golos, Tireless Pilgrim';
+const formattedString = searchedString
+  .toLowerCase()
+  .replace(',', '')
+  .replace(/\s/g, '-');
+
+const getTierNumber = async () => {
+  try {
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+    const lists = $('.boardlist');
+    for (let i = 0; i < lists.length; i++) {
+      const baseSelector = 'boardContainer-main-';
+      const fullId = `${baseSelector}${formattedString}`;
+      const children = lists[i]?.children;
+      for (let j = 0; j < children.length; j++) {
+        if (fullId === children[j]?.next?.attribs?.id) {
+          return `Tier ${i}`;
+        }
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  return 'Tier not found';
+};
+
+getTierNumber().then((resp) => console.log(resp));
